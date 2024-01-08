8b5077dc43f0271e22592a8dc539358e9061a317
BAWION <132302507+BAWION@users.noreply.github.com>
2024-01-09 02:25:22 +0300

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,13 +1,9 @@
 import os
 import logging
 from datetime import datetime
-import locale
 from telegram.ext import Updater, CommandHandler
 from news_parser import parse_news
 from translator import translate_text
-
-# Установка локали для корректного разбора даты
-locale.setlocale(locale.LC_TIME, 'en_US.UTF-8')
 
 # Настройка логирования
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -32,15 +28,9 @@ def send_news(update, context):
     last_published_article_file = 'last_published_article.txt'
     latest_article_date = datetime.min
 
-    new_articles = []
     for article in articles:
         article_date = datetime.strptime(article['date'], '%B %d, %Y')
         if is_new_article(article_date, last_published_article_file):
-            new_articles.append(article)
-            latest_article_date = max(latest_article_date, article_date)
-
-    if new_articles:
-        for article in new_articles:
             title = translate_text(article['title'])
             source = article['source']
             news_url = article['news_url']
@@ -48,8 +38,10 @@ def send_news(update, context):
 
             message = f"{title}\nИсточник: {source}\n[Читать далее]({news_url})\n![image]({image_url})"
             context.bot.send_message(chat_id=channel_name, text=message, parse_mode='Markdown')
+            latest_article_date = max(latest_article_date, article_date)
             logger.info(f"Отправляется новость: {title}")
 
+    if latest_article_date > datetime.min:
         update_last_published_article(latest_article_date, last_published_article_file)
 
 def send_latest_news(update, context):
