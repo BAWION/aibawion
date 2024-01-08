f365a52ffa49e1ba9b6eba7a65f6d0f30cd52034
BAWION <132302507+BAWION@users.noreply.github.com>
2024-01-08 16:39:25 +0300

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -3,43 +3,37 @@ from telegram.ext import Updater, CommandHandler
 from news_parser import parse_news
 from translator import translate_text
 
-def send_news(update, context):
-    channel_name = os.getenv('TELEGRAM_CHANNEL_NAME', '@your_default_channel_name')
+def is_new_article(article, last_published_article_file):
+    with open(last_published_article_file, 'r') as file:
+        last_published_article = file.read().strip()
+    return article['title'] != last_published_article
+
+def update_last_published_article(article, last_published_article_file):
+    with open(last_published_article_file, 'w') as file:
+        file.write(article['title'])
+
+def send_news_if_new():
     url = 'https://www.futuretools.io/news'
     articles = parse_news(url)
+    last_published_article_file = 'last_published_article.txt'
 
-    for article in articles:
+    if articles and is_new_article(articles[0], last_published_article_file):
+        article = articles[0]
         title = translate_text(article['title'])
         source = article['source']
         news_url = article['news_url']
         image_url = article['image_url']
 
         message = f"{title}\nИсточник: {source}\n[Читать далее]({news_url})\n![image]({image_url})"
+        channel_name = os.getenv('TELEGRAM_CHANNEL_NAME', '@your_channel_name')
         context.bot.send_message(chat_id=channel_name, text=message, parse_mode='Markdown')
-
-def send_latest_news(update, context):
-    channel_name = os.getenv('TELEGRAM_CHANNEL_NAME', '@your_default_channel_name')
-    url = 'https://www.futuretools.io/news'
-    articles = parse_news(url)
-
-    if articles:
-        article = articles[0]  # Берем первую новость из списка
-        title = translate_text(article['title'])
-        source = article['source']
-        news_url = article['news_url']
-        image_url = article['image_url']
-
-        message = f"{title}\nИсточник: {source}\n[Читать далее]({news_url})\n![image]({image_url})"
-        context.bot.send_message(chat_id=channel_name, text=message, parse_mode='Markdown')
-    else:
-        context.bot.send_message(chat_id=channel_name, text="Новости отсутствуют.")
+        update_last_published_article(article, last_published_article_file)
 
 def main():
-    token = os.getenv('TELEGRAM_BOT_TOKEN')  # Получение токена из переменных окружения
+    token = os.getenv('TELEGRAM_BOT_TOKEN')
     updater = Updater(token, use_context=True)
     dp = updater.dispatcher
-    dp.add_handler(CommandHandler('sendnews', send_news))
-    dp.add_handler(CommandHandler('latestnews', send_latest_news))  # Команда для отправки последней новости
+    dp.add_handler(CommandHandler('sendnews', send_news_if_new))  # Обновленный обработчик команд
     updater.start_polling()
     updater.idle()
 
