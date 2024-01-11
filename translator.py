2caeff722d6b9358afda9f33355a13399b1aec88
BAWION <132302507+BAWION@users.noreply.github.com>
2024-01-11 11:33:36 +0300

    Update translator.py

--- translator.py
+++ translator.py
@@ -1,17 +1,16 @@
 import openai
 
-# Установите ваш API ключ
-openai.api_key = 'YOUR_API_KEY'
-
-def translate_text(text, target_language='en'):
+def translate_text(api_key, text, target_language='en'):
     try:
+        openai.api_key = api_key
         response = openai.Completion.create(
             engine="text-davinci-002",
             prompt=f"Translate the following text to {target_language}: {text}",
-            max_tokens=50  # Укажите максимальное количество токенов для перевода
+            max_tokens=50
         )
         translation = response.choices[0].text.strip()
         return translation
     except Exception as e:
         print(f"Ошибка при переводе текста: {str(e)}")
         return text
+
