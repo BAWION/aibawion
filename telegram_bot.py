77bd71377beb4653df2d49846ece1ffb02612304
BAWION <132302507+BAWION@users.noreply.github.com>
2024-01-15 03:13:54 +0300

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -181,7 +181,7 @@ def main():
         # Настройка планировщика для регулярной отправки новостей
         scheduler = BackgroundScheduler(timezone=pytz.utc)
         job = partial(send_news, context=updater.job_queue)
-        scheduler.add_job(job, 'interval', minutes=3)
+        scheduler.add_job(job, 'interval', minutes=150)
         scheduler.start()
 
         updater.start_polling()
